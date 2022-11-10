d6de4acb3f28c1e2065e9afe358a64fe7a0b636c
stephenHahn88 <89817088+stephenHahn88@users.noreply.github.com>
2022-11-10 14:03:41 -0500

    preset transition matrices from server

--- ui/src/views/dagOther/HarmonicProgression.vue
+++ ui/src/views/dagOther/HarmonicProgression.vue
@@ -2,50 +2,74 @@
   <div>
     <h1>Harmonic Progression</h1>
     <b-row class="ml-2">
-<!--      TRANSITION MATRIX-->
+<!--      TRANSITION MATRIX BLOCK-->
       <b-col
           class="p-2"
           cols="6"
           style="background-color: rgba(0, 0, 0, 0.3); border-radius: 10px; overflow: auto">
-        <h2>Transition Matrix</h2>
-        <b-container style="width: 640px; border-radius: 10px;">
-          <b-row>
-            <b-col class="mt-3">
-              <b-button variant="danger" @click="clear">X</b-button>
-            </b-col>
-            <b-col
-                v-for="(color, harmony_label, i) in harmonies" :key="i"
-                style="text-align: center"
-                class="mt-3"
-            >
-              <p
-                  :style='{ backgroundColor: harmonies[harmony_label], color: "white", borderRadius: "30px"}'
-              >{{harmony_label}}</p>
-            </b-col>
-          </b-row>
-          <b-row
-              v-for="(color, harmony_from, i) in harmonies" :key="i"
-              class="harmony_row"
-              :class="harmony_from"
+        <b-row class="mb-5" style="height: 100px;">
+          <b-col cols="4"><h2>Transition Matrix</h2></b-col>
+<!--          PRESET MATRICES-->
+          <b-col
+              class="mr-3"
+              style="
+                background-color: rgba(255, 255, 255, 0.2);
+                border-radius: 10px;
+              "
           >
-            <b-col
-              style="text-align: center"
+            <h3>Preset Matrices</h3>
+            <b-button
+              v-for="preset in presetMatrices"
+              variant="info"
+              style="width: 100px;"
+              class="m-2"
+              @click="getMatrix(preset)"
             >
-              <p
-                  :style='{ backgroundColor: harmonies[harmony_from], color: "white", borderRadius: "30px"}'
-              >{{harmony_from}}</p>
-            </b-col>
-            <b-col
-                v-for="(color, harmony_to, j) in harmonies" :key="j"
+              {{preset}}
+            </b-button>
+          </b-col>
+        </b-row>
+<!--        MATRIX-->
+        <b-row>
+          <b-container style="width: 640px; border-radius: 10px;">
+            <b-row>
+              <b-col class="mt-3">
+                <b-button variant="danger" @click="clear">X</b-button>
+              </b-col>
+              <b-col
+                  v-for="(color, harmony_label, i) in harmonies" :key="i"
+                  style="text-align: center"
+                  class="mt-3"
+              >
+                <p
+                    :style='{ backgroundColor: harmonies[harmony_label], color: "white", borderRadius: "30px"}'
+                >{{harmony_label}}</p>
+              </b-col>
+            </b-row>
+            <b-row
+                v-for="(color, harmony_from, i) in harmonies" :key="i"
+                class="harmony_row"
+                :class="harmony_from"
             >
-              <b-form-input
-                  class="my-2"
-                  style="width:48px;"
-                  v-model="transitions[harmony_from][harmony_to]"
-              ></b-form-input>
-            </b-col>
-          </b-row>
-        </b-container>
+              <b-col
+                style="text-align: center"
+              >
+                <p
+                    :style='{ backgroundColor: harmonies[harmony_from], color: "white", borderRadius: "30px"}'
+                >{{harmony_from}}</p>
+              </b-col>
+              <b-col
+                  v-for="(color, harmony_to, j) in harmonies" :key="j"
+              >
+                <b-form-input
+                    class="my-2"
+                    style="width:48px;"
+                    v-model="transitions[harmony_from][harmony_to]"
+                ></b-form-input>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-row>
       </b-col>
 <!--      SEQUENCE GENERATION-->
       <b-col
@@ -129,24 +153,26 @@ import "@livereader/graphly-d3/style.css";
 
 import Hexagon from "../../static/hexagon"
 
-let harmonies = {
+let harmonies = ref({
   'I': 'green',
-  'ii': 'navy',
-  'iii': 'rgb(255, 130, 200)',
+  'II': 'navy',
+  'III': 'rgb(255, 130, 200)',
   'IV': 'orange',
   "V": 'red',
-  "vi": 'purple',
-  "vii": 'black'
-}
+  "VI": 'purple',
+  "VII": 'black'
+})
+
+let presetMatrices = ["Classical Major", "Classical Minor", "Rock", "Pentatonic"]
 
 let transitions = ref({
-  'I': ref({'I': 0, 'ii': 0.2, 'iii': 0.1, 'IV': 0.2, "V": 0.2, "vi": 0.2, "vii": 0.1}),
-  'ii': ref({'I': 0, 'ii': 0, 'iii': 0, 'IV': 0, "V": 1, "vi": 0, "vii": 0}),
-  'iii': ref({'I': 0, 'ii': 0.1, 'iii': 0, 'IV': 0.6, "V": 0, "vi": 0.3, "vii": 0}),
-  'IV': ref({'I': 0, 'ii': 0.4, 'iii': 0, 'IV': 0, "V": 0.5, "vi": 0, "vii": 0.1}),
-  'V': ref({'I': 0.8, 'ii': 0, 'iii': 0, 'IV': 0, "V": 0, "vi": 0.2, "vii": 0}),
-  'vi': ref({'I': 0, 'ii': 0.5, 'iii': 0.1, 'IV': 0.3, "V": 0.1, "vi": 0, "vii": 0}),
-  'vii': ref({'I': 1.0, 'ii': 0, 'iii': 0, 'IV': 0, "V": 0, "vi": 0, "vii": 0})
+  'I': ref({'I': 0, 'II': 0.2, 'III': 0.1, 'IV': 0.2, "V": 0.2, "VI": 0.2, "VII": 0.1}),
+  'II': ref({'I': 0, 'II': 0, 'III': 0, 'IV': 0, "V": 1, "VI": 0, "VII": 0}),
+  'III': ref({'I': 0, 'II': 0.1, 'III': 0, 'IV': 0.6, "V": 0, "VI": 0.3, "VII": 0}),
+  'IV': ref({'I': 0, 'II': 0.4, 'III': 0, 'IV': 0, "V": 0.5, "VI": 0, "VII": 0.1}),
+  'V': ref({'I': 0.8, 'II': 0, 'III': 0, 'IV': 0, "V": 0, "VI": 0.2, "VII": 0}),
+  'VI': ref({'I': 0, 'II': 0.5, 'III': 0.1, 'IV': 0.3, "V": 0.1, "VI": 0, "VII": 0}),
+  'VII': ref({'I': 1.0, 'II': 0, 'III': 0, 'IV': 0, "V": 0, "VI": 0, "VII": 0})
 })
 
 let mySVG;
@@ -159,6 +185,40 @@ let sequences = ref({
   "c": ref({"sequenceReverse": [], "maxLen": 5, 'selected': 'inactive'}),
   "d": ref({"sequenceReverse": [], "maxLen": 3, 'selected': 'inactive'}),
 })
+
+async function getMatrix(preset) {
+  preset = preset.replaceAll(" ", "_")
+  let matrix = await fetch(`/model/matrix/${preset}`)
+  matrix = await matrix.json()
+
+  let mat = matrix["matrix"]
+  let labels = [...matrix["labels"]]
+  for (let [i, l] of labels.entries()) {
+    labels[i] = l.replace(/[\[\]no1234567890]/g, "").toUpperCase()
+  }
+  console.log(labels)
+
+  // TRANSPOSE MATRIX
+  mat = _.zip(...mat)
+  console.log(mat)
+  // PLACE VALUES INCLUDED IN RETRIEVED MATRIX
+  for (let [i, row] of mat.entries()) {
+    for (let [j, val] of row.entries()) {
+      transitions.value[labels[i]][labels[j]] = val
+    }
+  }
+  // PLACE ZEROS ELSEWHERE
+  for (let label in harmonies.value) {
+    if (!labels.includes(label)) {
+      for (let label2 in harmonies.value) {
+        transitions.value[label][label2] = 0
+        transitions.value[label2][label] = 0
+      }
+    }
+  }
+  console.log(transitions.value)
+  redraw()
+}
 
 function selectRow(letter) {
   for (let l in sequences.value) {
@@ -194,24 +254,25 @@ function erase() {
 }
 
 onMounted(() => {
-  harmonies = {
+  harmonies = ref({
     'I': 'green',
-    'ii': 'navy',
-    'iii': 'rgb(255, 130, 200)',
+    'II': 'navy',
+    'III': 'rgb(255, 130, 200)',
     'IV': 'orange',
     "V": 'red',
-    "vi": 'purple',
-    "vii": 'black'
-  }
-  let transitions = ref({
-    'I': ref({'I': 0, 'ii': 0.2, 'iii': 0.1, 'IV': 0.2, "V": 0.2, "vi": 0.2, "vii": 0.1}),
-    'ii': ref({'I': 0, 'ii': 0, 'iii': 0, 'IV': 0, "V": 1, "vi": 0, "vii": 0}),
-    'iii': ref({'I': 0, 'ii': 0.1, 'iii': 0, 'IV': 0.6, "V": 0, "vi": 0.3, "vii": 0}),
-    'IV': ref({'I': 0, 'ii': 0.4, 'iii': 0, 'IV': 0, "V": 0.5, "vi": 0, "vii": 0.1}),
-    'V': ref({'I': 0.8, 'ii': 0, 'iii': 0, 'IV': 0, "V": 0, "vi": 0.2, "vii": 0}),
-    'vi': ref({'I': 0, 'ii': 0.5, 'iii': 0.1, 'IV': 0.3, "V": 0.1, "vi": 0, "vii": 0}),
-    'vii': ref({'I': 1.0, 'ii': 0, 'iii': 0, 'IV': 0, "V": 0, "vi": 0, "vii": 0})
+    "VI": 'purple',
+    "VII": 'black'
   })
+
+  // transitions = ref({
+  //   'I': ref({'I': 0, 'II': 0.2, 'III': 0.1, 'IV': 0.2, "V": 0.2, "VI": 0.2, "VII": 0.1}),
+  //   'II': ref({'I': 0, 'II': 0, 'III': 0, 'IV': 0, "V": 1, "VI": 0, "VII": 0}),
+  //   'III': ref({'I': 0, 'II': 0.1, 'III': 0, 'IV': 0.6, "V": 0, "VI": 0.3, "VII": 0}),
+  //   'IV': ref({'I': 0, 'II': 0.4, 'III': 0, 'IV': 0, "V": 0.5, "VI": 0, "VII": 0.1}),
+  //   'V': ref({'I': 0.8, 'II': 0, 'III': 0, 'IV': 0, "V": 0, "VI": 0.2, "VII": 0}),
+  //   'VI': ref({'I': 0, 'II': 0.5, 'III': 0.1, 'IV': 0.3, "V": 0.1, "VI": 0, "VII": 0}),
+  //   'VII': ref({'I': 1.0, 'II': 0, 'III': 0, 'IV': 0, "V": 0, "VI": 0, "VII": 0})
+  // })
 
   mySVG = document.getElementById("mySVG");
   simulation = new ForceSimulation(mySVG);
@@ -222,7 +283,7 @@ onMounted(() => {
 
 function redraw() {
   let nodes = []
-  for (let harmony in harmonies) {
+  for (let harmony in harmonies.value) {
     nodes.push({
       id: harmony,
       shape: {
@@ -231,7 +292,7 @@ function redraw() {
       },
       payload: {
         title: harmony,
-        color: harmonies[harmony]
+        color: harmonies.value[harmony]
       }
     })
   }
@@ -285,7 +346,7 @@ p {
   font-size: 24px;
 }
 
-p, h1, h2, col {
+p, h1, h2, h3, col {
   color: white;
 }
 
